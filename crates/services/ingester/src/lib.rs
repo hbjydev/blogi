5fe4a2c7167a3edecc2f28210b1b870bf160000f
Hayden Young <hayden@hayden.moe>
2025-08-17 17:26:13 +0100

    fix: simplify return

--- crates/services/ingester/src/lib.rs
+++ crates/services/ingester/src/lib.rs
@@ -2,8 +2,6 @@ use std::{collections::HashMap, sync::{Arc, Mutex}};
 
 use anyhow::Result;
 use async_trait::async_trait;
-use blogi_errors::BlogiError;
-use blogi_lexicons::moe::hayden::blogi::actor::Profile;
 use rocketman::{connection::JetstreamConnection, handler, ingestion::LexiconIngestor, options::JetstreamOptions, types::event::Event};
 use serde_json::Value;
 use tracing::info;
@@ -19,23 +17,17 @@ pub async fn start(_datastore: Box<dyn blogi_db::Datastore>) -> Result<()> {
 
     let jetstream = JetstreamConnection::new(opts);
 
-    // create your ingestors
     let mut ingestors: HashMap<String, Box<dyn LexiconIngestor + Send + Sync>> = HashMap::new();
     ingestors.insert(
-        // your EXACT nsid
         "moe.hayden.blogi.actor.profile".to_string(),
         Box::new(ProfileIngestor),
     );
 
-    // tracks the last message we've processed
     let cursor: Arc<Mutex<Option<u64>>> = Arc::new(Mutex::new(None));
 
-    // get channels
     let msg_rx = jetstream.get_msg_rx();
     let reconnect_tx = jetstream.get_reconnect_tx();
 
-    // spawn a task to process messages from the queue.
-    // this is a simple implementation, you can use a more complex one based on needs.
     let c_cursor = cursor.clone();
     tokio::spawn(async move {
         while let Ok(message) = msg_rx.recv_async().await {
@@ -48,12 +40,10 @@ pub async fn start(_datastore: Box<dyn blogi_db::Datastore>) -> Result<()> {
         }
     });
 
-    Ok(
-        jetstream
-            .connect(cursor.clone())
-            .await
-            .map_err(|e| anyhow::anyhow!("error running ingest: {}", e))?
-    )
+    jetstream
+        .connect(cursor.clone())
+        .await
+        .map_err(|e| anyhow::anyhow!("error running ingest: {}", e))
 }
 
 pub struct ProfileIngestor;
